442a65575117539cfa68d03ae1251fbae21b949d
ライアン <ryan.wachowski@duethealth.com>
2018-11-11 02:51:01 -0500

    Trying to fix the module definition

--- Raptor/Sources/include/raptor.h
+++ Raptor/Sources/include/raptor.h
@@ -12,6 +12,12 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
+    
+    extern int TYPE_SERVERDATA_AUTH;
+    extern int TYPE_SERVERDATA_EXECCOMMAND;
+    extern int TYPE_SERVERDATA_AUTH_RESPONSE;
+    extern int TYPE_SERVERDATA_RESPONSE_VALUE;
+    
     void* srcon_create(char* address, int port, char* password);
     const char* srcon_send(void* srcon, char* message, int type);
 #ifdef __cplusplus
--- Raptor/Sources/src/raptor.cpp
+++ Raptor/Sources/src/raptor.cpp
@@ -6,8 +6,13 @@
 //  Copyright © 2018 Craft of the Mines. All rights reserved.
 //
 
-#include "../include/umbrella.h"
+#include "../include/raptor.h"
 #include "srcon.h"
+
+int TYPE_SERVERDATA_AUTH = 3;
+int TYPE_SERVERDATA_EXECCOMMAND = 2;
+int TYPE_SERVERDATA_AUTH_RESPONSE = 2;
+int TYPE_SERVERDATA_RESPONSE_VALUE = 0;
 
 void* srcon_create(char* address, int port, char* password) {
     return new srcon(std::string(address), port, std::string(password));
